123e3a6593f7ec4f7fab68d5b52fe75d5105fbc3
Yassine Nifa <yasine.nifa@gmail.com>
2021-02-16 08:39:52 +0100

    connect to firebase + create the header

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    // BEM
+    <div className="app">
+      { /*Header*/ }
+      <Header />
+      { /*Home*/ }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAeIWD_ZZ3L4-mnYvJTFKzftjwcQvNvV8Q",
+    authDomain: "clone-559fa.firebaseapp.com",
+    projectId: "clone-559fa",
+    storageBucket: "clone-559fa.appspot.com",
+    messagingSenderId: "419086735851",
+    appId: "1:419086735851:web:5aee96b42f73f2856e61ae",
+    measurementId: "G-2BC4G24PTQ"
+  };
