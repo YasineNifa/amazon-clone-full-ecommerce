62ba9ed818a88b4d3012e1becad9449884f9f042
Yassine Nifa <yasine.nifa@gmail.com>
2021-02-16 18:28:06 +0100

    Create the home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './Header';
+import Home from './Home';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
     <div className="app">
       { /*Header*/ }
       <Header />
+      <Home />
       { /*Home*/ }
     </div>
   );
