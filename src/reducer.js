4a75cedc1d9eaea2f60d45cab84d4e46210df9a8
Yassine Nifa <yasine.nifa@gmail.com>
2021-02-17 13:30:15 +0100

    Create THE CHECKOUT Page

--- src/reducer.js
+++ src/reducer.js
@@ -15,6 +15,20 @@ export const initialState = {
                 ...state,
                 basket:[...state.basket,action.item]
             };
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+            if(index >= 0){
+                newBasket.splice(index,1);
+            }else{
+                console.warn(`Can't remove product (id:${action.id}) as its not in basket`)
+            }
+            return {
+                ...state,
+                basket : newBasket
+            }
       default:
         return state;
     }
